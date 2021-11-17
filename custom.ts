508291dda3be1aad3425ceecae6ffd65e9a32ad8
唐杰 <51323906+tangjie133@users.noreply.github.com>
2021-11-17 13:12:52 +0800

    更新 pxt.json, custom.ts

---
+++ custom.ts
@@ -0,0 +1,23 @@
+
+
+
+
+/**
+ * Custom blocks
+ */
+//% weight=100 color=#0fbc11 icon=""
+namespace custom {
+    /**
+     * TODO: describe your function here
+     * @param n describe parameter here, eg: 5
+     * @param s describe parameter here, eg: "Hello"
+     * @param e describe parameter here
+     */
+   
+
+    /**
+     * TODO: describe your function here
+     * @param value describe value here, eg: 5
+     */
+   
+}
